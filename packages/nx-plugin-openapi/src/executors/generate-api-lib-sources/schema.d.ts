fe919ce288aafd84e074a80e7894d41bfe0f22e3
istomerf <omer.feigenbaum@crowdcode.io>
2026-01-29 19:27:42 +0100

    Update schema.d.ts

--- packages/nx-plugin-openapi/src/executors/generate-api-lib-sources/schema.d.ts
+++ packages/nx-plugin-openapi/src/executors/generate-api-lib-sources/schema.d.ts
@@ -1,5 +1,6 @@
 export interface GenerateApiLibSourcesExecutorSchema {
   generator: string;
+  outputDir: string;
   sourceSpecPathOrUrl: string;
   useDockerBuild?: boolean;
   sourceSpecUrlAuthorizationHeaders?: string;
